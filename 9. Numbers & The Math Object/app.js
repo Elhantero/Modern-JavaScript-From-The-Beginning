cc10790dd77210e782d562a740866e13a382959b
a17-Fesenko <elhantero@gmail.com>
2018-06-04 22:12:02 +0300

    9. Numbers & The Math Object

---
+++ 9. Numbers & The Math Object/app.js
@@ -0,0 +1,27 @@
+const num1 = 100;
+const num2 = 50;
+let val;
+
+// Simple math with numbers
+val = num1 + num2;
+val = num1 - num2;
+val = num1 * num2;
+val = num1 / num2;
+val = num1 % num2;
+
+// Math objects
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.4);
+val = Math.ceil(2.1);
+val = Math.floor(2.8);
+val = Math.sqrt(64);
+val = Math.abs(-3);
+val = Math.pow(2, 3);
+val = Math.min(2, 5, 6, 9, 1, 44);
+val = Math.max(2, 5, 6, 9, 1, 44);
+val = Math.random();
+val = Math.random() * 10;
+val = Math.floor(Math.random() * 10 + 1);
+
+console.log(val);
