a07bf6531bcb902e21dd70c04584b2ae18a9ab83
a17-Fesenko <elhantero@gmail.com>
2018-06-28 09:38:06 +0300

    24. DOM Selectors For Multiple Elements

--- 23. DOM Selectors For Single Elements/app.js
+++ 23. DOM Selectors For Single Elements/app.js
@@ -1,21 +1,35 @@
 // document.getElementById()
 
-console.log(document.getElementById("task-title"));
+// console.log(document.getElementById('task-title'));
 
-// Get things from the element
-console.log(document.getElementById("task-title").id);
-console.log(document.getElementById("task-title").className);
+// // Get things from the element
+// console.log(document.getElementById('task-title').id);
+// console.log(document.getElementById('task-title').className);
 
-// Change styling
-document.getElementById("task-title").style.background = "green";
-document.getElementById("task-title").style.color = "red";
+// const taskTitle = document.getElementById('task-title');
 
-// Change content
-document.getElementById("task-title").textContent = "task list";
-document.getElementById("task-title").innerText = "My tasksa";
-document.getElementById("task-title").innerHTML =
-  '<span style="color:blue">Task list</span>';
+// // Change styling
+// taskTitle.style.background = '#333';
+// taskTitle.style.color = '#fff';
+// taskTitle.style.padding = '5px';
+// // taskTitle.style.display = 'none';
+
+// // Change content
+// taskTitle.textContent = 'Task List';
+// taskTitle.innerText = 'My Tasks';
+// taskTitle.innerHTML = '<span style="color:red">Task List</span>';
 
 // document.querySelector()
+
 console.log(document.querySelector("#task-title"));
 console.log(document.querySelector(".card-title"));
+console.log(document.querySelector("h5"));
+
+document.querySelector("li").style.color = "red";
+document.querySelector("ul li").style.color = "blue";
+
+document.querySelector("li:last-child").style.color = "red";
+document.querySelector("li:nth-child(3)").style.color = "yellow";
+document.querySelector("li:nth-child(4)").textContent = "Hello World";
+document.querySelector("li:nth-child(odd)").style.background = "#ccc";
+document.querySelector("li:nth-child(even)").style.background = "#f4f4f4";
